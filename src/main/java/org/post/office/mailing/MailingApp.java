ac023a82c260b9bd2e2c548cee1237e3e7e47d8d
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2021-04-28 13:35:11 +0300

    Project init

---
+++ src/main/java/org/post/office/mailing/MailingApp.java
@@ -0,0 +1,11 @@
+package org.post.office.mailing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailingApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MailingApp.class, args);
+    }
+}
