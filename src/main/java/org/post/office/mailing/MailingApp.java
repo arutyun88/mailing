ba7d140bbc68767cae2c3c0e47fc42b1bcc61fea
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2021-04-30 17:18:55 +0300

    webapp added

--- src/main/java/org/post/office/mailing/MailingApp.java
+++ src/main/java/org/post/office/mailing/MailingApp.java
@@ -2,10 +2,17 @@ package org.post.office.mailing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MailingApp {
+public class MailingApp extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(MailingApp.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MailingApp.class);
     }
 }
