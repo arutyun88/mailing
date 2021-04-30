16f6105659670770045ddf6ff6c177dd1e689d99
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2021-04-30 12:50:51 +0300

    Get all sending methods realized

--- src/main/java/org/post/office/mailing/controller/PostalItemController.java
+++ src/main/java/org/post/office/mailing/controller/PostalItemController.java
@@ -1,17 +1,19 @@
 package org.post.office.mailing.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.post.office.mailing.model.entity.PostalItemEntity;
-import org.post.office.mailing.model.entity.PostalOfficeEntity;
 import org.post.office.mailing.repository.PostalItemRepository;
-import org.post.office.mailing.service.PostalItemService;
-import org.post.office.mailing.service.PostalOfficeServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.Collections;
 import java.util.List;
 
+@Slf4j
 @RestController
 public class PostalItemController {
     private final PostalItemRepository repository;
@@ -20,7 +22,7 @@ public class PostalItemController {
         this.repository = repository;
     }
 
-    @GetMapping("actual")
+    @GetMapping("item/actual")
     public List<PostalItemEntity> getAllActualPostalItems() {
         try {
             return repository.findAllWhereDeletedFalse();
@@ -29,12 +31,32 @@ public class PostalItemController {
         }
     }
 
-    @GetMapping("archive")
+    @GetMapping("item/archive")
     public List<PostalItemEntity> getAllArchivePostalItems() {
         try {
             return repository.findAllWhereDeletedTrue();
         } catch (EntityNotFoundException exception) {
             return Collections.emptyList();
+        }
+    }
+
+    @GetMapping("item/getAll")
+    public List<PostalItemEntity> getAllPostalItems() {
+        try {
+            return repository.findAll();
+        } catch (EntityNotFoundException exception) {
+            return Collections.emptyList();
+        }
+    }
+
+    @GetMapping("item/{postalItem}")
+    public ResponseEntity<?> getPostalItemById(@PathVariable("postalItem") long postalItem) {
+        PostalItemEntity result = repository.findById(postalItem).orElse(null);
+        if (result != null) {
+            return ResponseEntity.ok().body(result);
+        } else {
+            log.warn("Postal item " + postalItem + " not found!");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
 }
